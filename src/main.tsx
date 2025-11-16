d2285bf6f63ee848037fc726fc9960cd90e823b4
Victor Owiti <101042985+vicowiti@users.noreply.github.com>
2025-11-16 20:38:43 +0300

    Project skeleton setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
---
+++ src/pages/MyTeam.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyTeam = () => {
+  return <div>MyTeam</div>;
+};
+
+export default MyTeam;
